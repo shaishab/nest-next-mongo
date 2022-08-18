36b81b090d964be50c8484f01c7b18495344cd81
Shaishab <shaishab.cse@gmail.com>
2022-08-18 22:57:19 +0600

    Added dev and prod env fiel and update app.module to use env variable

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { ViewModule } from './modules/view/view.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.dev', '.env.prod'],
+    }),
     ViewModule,
-    MongooseModule.forRoot('mongodb://localhost/nest-next'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [],
   providers: [],
