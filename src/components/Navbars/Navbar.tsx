adedbc3f50a5eee7c3a66f346aa75faa86c481b4
Shaishab <shaishab.cse@gmail.com>
2022-09-14 23:38:59 +0600

    Updated header and Navbar

--- src/components/Navbars/Navbar.tsx
+++ src/components/Navbars/Navbar.tsx
@@ -4,31 +4,41 @@ import type { ReactNode } from 'react';
 import { AppConfig } from '../../utils/App.config';
 
 type INavbarProps = {
-  meta: ReactNode;
+    children?: ReactNode;
 };
 
 const Navbar = (props: INavbarProps) => (
     <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
-            <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-            <span className="font-semibold text-xl tracking-tight">{AppConfig.site_name}</span>
+            <img className="w-8 h-8 mr-2 rounded-full" width="54" height="54" src="/static/android-chrome-192x192.png" alt="image description"></img>
+            <Link href="/views/home" as="/">
+                <a>
+                    <span className="font-semibold text-xl tracking-tight">{AppConfig.site_name}</span>
+                </a>
+            </Link>
         </div>
         <div className="block lg:hidden">
             <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-                <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                <img className="w-8 h-8 mr-2 rounded-full" width="54" height="54" src="/static/android-chrome-192x192.png" alt="image description"></img>
             </button>
         </div>
         <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
             <div className="text-sm lg:flex-grow">
-                <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                Docs
-                </a>
-                <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-                Examples
-                </a>
-                <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-                Blog
-                </a>
+                <Link href="/views/home" as="/">
+                    <a className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                    Home
+                    </a>
+                </Link>
+                <Link href="/views/about" as="/about">
+                    <a className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                    About
+                    </a>
+                </Link>
+                <Link href="/views/blog" as="/blog" prefetch={false}>                
+                    <a className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+                    Blog
+                    </a>
+                </Link>
             </div>
             <div>
                 <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
