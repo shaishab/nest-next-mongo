929d9b04fdf77c9f34212a97ef2a6c05eae568a8
Shaishab <shaishab.cse@gmail.com>
2022-09-18 00:36:11 +0600

    Added functioanla navbar

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -6,6 +6,7 @@ import Meta from '../Meta/Meta';
 import Navbar from '../Navbars/Navbar';
 import Navbar1 from '../Navbars/Navbar1';
 import Navbar2 from '../Navbars/Navbar2';
+import Navbar3 from '../Navbars/Navbar3';
 
 type INavbarProps = {
   meta?: {
@@ -20,7 +21,7 @@ type INavbarProps = {
 const Header = (props: INavbarProps) => (
   <>
     <Meta {...props.meta}/>
-    <Navbar2 {...props}/>
+    <Navbar3 {...props}/>
   </>
     
 );
