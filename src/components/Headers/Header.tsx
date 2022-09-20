22b7bc8f3894b79dac2142342170deacc76e0bc4
Shaishab <bs272@shaishab-MacBook-Pro-BS272.local>
2022-09-20 17:47:27 +0600

    Navbar aria-current issue fix

--- src/components/Headers/Header.tsx
+++ src/components/Headers/Header.tsx
@@ -1,12 +1,8 @@
 import Link from 'next/link';
 import type { ReactNode } from 'react';
 
-import { AppConfig } from '../../utils/App.config';
 import Meta from '../Meta/Meta';
 import Navbar from '../Navbars/Navbar';
-import Navbar1 from '../Navbars/Navbar1';
-import Navbar2 from '../Navbars/Navbar2';
-import Navbar3 from '../Navbars/Navbar3';
 
 type INavbarProps = {
   meta?: {
@@ -21,7 +17,7 @@ type INavbarProps = {
 const Header = (props: INavbarProps) => (
   <>
     <Meta {...props.meta}/>
-    <Navbar3 {...props}/>
+    <Navbar {...props}/>
   </>
     
 );
